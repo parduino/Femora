6287c0f76303595c41917849060c24987f7eba1d
amnp95 <amnp95@uw.edu>
2024-12-19 13:02:38 -0800

    edititng debug models

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def main():
         from drm_analyzer.components.Element.elementsOpenSees import stdBrickElement
         from drm_analyzer.components.Mesh.meshPartInstance import StructuredRectangular3D
 
-
-        elastic  = ElasticIsotropicMaterial(user_name="Steel", E=200e3, ν=0.3,  ρ=7.85e-9)
-        stdbrik1 = stdBrickElement(ndof=3, material=elastic, b1=0, b2=0, b3=-10)
-        stdbrik2 = stdBrickElement(ndof=3, material=elastic, b1=0, b2=0, b3=-10)
-        stdbrik3 = stdBrickElement(ndof=3, material=elastic, b1=0, b2=0, b3=-10)
+        elastic1  = ElasticIsotropicMaterial(user_name="Steel1", E=200e3, ν=0.3,  ρ=7.85e-9)
+        elastic2  = ElasticIsotropicMaterial(user_name="Steel2", E=200e3, ν=0.3,  ρ=7.85e-9)
+        stdbrik1 = stdBrickElement(ndof=3, material=elastic1, b1=0, b2=0, b3=-10)
+        stdbrik2 = stdBrickElement(ndof=3, material=elastic2, b1=0, b2=0, b3=-10)
+        stdbrik3 = stdBrickElement(ndof=3, material=elastic2, b1=0, b2=0, b3=-10)
         StructuredRectangular3D(user_name="base",    element=stdbrik1, **{'X Min': -50, 'X Max': 50, 'Y Min': -50, 'Y Max': 50, 'Z Min': -30, 'Z Max': -20, 'Nx Cells': 100, 'Ny Cells': 100, 'Nz Cells': 10})
         StructuredRectangular3D(user_name="middle",  element=stdbrik2, **{'X Min': -50, 'X Max': 50, 'Y Min': -50, 'Y Max': 50, 'Z Min': -20, 'Z Max': -10, 'Nx Cells': 100, 'Ny Cells': 100, 'Nz Cells': 10})
         StructuredRectangular3D(user_name="top",     element=stdbrik3, **{'X Min': -50, 'X Max': 50, 'Y Min': -50, 'Y Max': 50, 'Z Min': -10, 'Z Max':   0, 'Nx Cells': 100, 'Ny Cells': 100, 'Nz Cells': 10})
