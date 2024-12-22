1fbeb4bda7088e6c29033f725cadbfdb419052c3
amnp95 <amnp95@uw.edu>
2024-12-21 20:57:38 -0800

    edit the debug model

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ os.environ["QT_API"] = "pyside6"
 import sys
 from qtpy.QtWidgets import QApplication
 from meshmaker.gui.main_window import MainWindow
-import qtpy
+
 
 DEBUG = True
 
@@ -15,17 +15,18 @@ def main():
         from meshmaker.components.Material.materialsOpenSees import ElasticIsotropicMaterial
         from meshmaker.components.Element.elementsOpenSees import stdBrickElement
         from meshmaker.components.Mesh.meshPartInstance import StructuredRectangular3D
-
         elastic1  = ElasticIsotropicMaterial(user_name="Steel1", E=200e3, ν=0.3,  ρ=7.85e-9)
         elastic2  = ElasticIsotropicMaterial(user_name="Steel2", E=200e3, ν=0.3,  ρ=7.85e-9)
+        elsatic3  = ElasticIsotropicMaterial(user_name="Steel3", E=400e3, ν=0.25, ρ=7.85e-9)
         stdbrik1 = stdBrickElement(ndof=3, material=elastic1, b1=0, b2=0, b3=-10)
         stdbrik2 = stdBrickElement(ndof=3, material=elastic2, b1=0, b2=0, b3=-10)
         stdbrik3 = stdBrickElement(ndof=3, material=elastic2, b1=0, b2=0, b3=-10)
         StructuredRectangular3D(user_name="base",    element=stdbrik1, **{'X Min': -50, 'X Max': 50, 'Y Min': -50, 'Y Max': 50, 'Z Min': -30, 'Z Max': -20, 'Nx Cells': 100, 'Ny Cells': 100, 'Nz Cells': 10})
         StructuredRectangular3D(user_name="middle",  element=stdbrik2, **{'X Min': -50, 'X Max': 50, 'Y Min': -50, 'Y Max': 50, 'Z Min': -20, 'Z Max': -10, 'Nx Cells': 100, 'Ny Cells': 100, 'Nz Cells': 10})
         StructuredRectangular3D(user_name="top",     element=stdbrik3, **{'X Min': -50, 'X Max': 50, 'Y Min': -50, 'Y Max': 50, 'Z Min': -10, 'Z Max':   0, 'Nx Cells': 100, 'Ny Cells': 100, 'Nz Cells': 10})
-    # ========================================
-    print(f"Using Qt bindings: {qtpy.API_NAME}")
+        # ========================================
+        import qtpy
+        print(f"Using Qt bindings: {qtpy.API_NAME}")
 
 
     app = QApplication(sys.argv)
