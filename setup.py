56c2c4e592be7c552636b9eccb739ae028fdfbd3
amnp95 <amnp95@uw.edu>
2025-04-22 17:24:30 -0700

    refactor: update package name from 'meshmaker' to 'femora' in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="meshmaker",
+    name="femora",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
