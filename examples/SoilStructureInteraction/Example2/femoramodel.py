46e8221eca51c1b3d1c18c7f8df85b95c4c7b379
Amin Pakzad <amnp95@uw.edu>
2025-08-11 16:10:17 -0700

    fix(femoramodel): enable time step for embedded beam solid interface recorder

--- examples/SoilStructureInteraction/Example2/femoramodel.py
+++ examples/SoilStructureInteraction/Example2/femoramodel.py
@@ -252,8 +252,7 @@ fm.drm.createDefaultProcess(
 
 recorder = fm.recorder.embedded_beam_solid_interface(
     interface=["pile_soil_interface1", "pile_soil_interface2"],
-    resp_type=["displacement", "axialDisp"],
-    #dt=0.005
+    dt=0.005
     )
 fm.process.insert_step(index=-2,
                        component= recorder,
